09ccaf0f67e3f9e4e1a0e6c9b99032985f558287
Suyog Patil <suyog0057@gmail.com>
2024-08-09 17:55:55 +0530

    fix: teacher grading other teacher

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -79,6 +79,7 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(auth_principal.teacher_id == assignment.teacher_id)
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
